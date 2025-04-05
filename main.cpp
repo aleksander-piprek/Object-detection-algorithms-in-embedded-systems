79066a024e7ee9eb9445cadfa4b03bcc1e5d35e7
Aleksander Piprek <kerpipkelo@gmail.com>
2025-04-05 18:08:04 +0200

    Added window class

--- main.cpp
+++ main.cpp
@@ -1,20 +1,7 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
+#include "src/window/window.hpp"
 
 int main()
 {
-    cv::Mat image = cv::Mat::zeros(480, 640, CV_8UC3);
-
-    cv::putText(image, 
-                "Hello, OpenCV!", 
-                cv::Point(50, 100), 
-                cv::FONT_HERSHEY_SIMPLEX, 
-                1.0, 
-                cv::Scalar(255, 255, 255), 
-                2);
-
-    cv::imshow("Image", image);
-    cv::waitKey(0);
-
+    Window window;
     return 0;
 }
