5eb653b81ac48863150a6f9c121d445cbc7077d2
Aleksander Piprek <kerpipkelo@gmail.com>
2025-04-06 21:48:53 +0200

    Sandbox development

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -8,13 +8,43 @@ class Sandbox
     public:
         Sandbox() = default;
         void play();
+        
+        void imageResize();
+        void imageGrayscale();
+        void imageGrayscaleTransition();
 };
 
 void Sandbox::play()
+{
+    imageGrayscale();
+}
+
+void Sandbox::imageResize()
 {
     Vision::Image image("../resources/images/Wagtail.JPG");
     Vision::Image resized;
 
     cv::resize(image.getImage(), resized.getImage(), cv::Size(1280, 600));
     resized.showImage();
+}
+
+void Sandbox::imageGrayscale()
+{
+    Vision::Image image("../resources/images/Wagtail.JPG");
+    Vision::Image gray;
+
+    cv::cvtColor(image.getImage(), gray.getImage(), cv::COLOR_BGR2GRAY);
+    gray.showImage();
+}
+
+void Sandbox::imageGrayscaleTransition()
+{
+    Vision::Image image("../resources/images/Wagtail.JPG");
+    Vision::Image transitions;
+
+    for(auto& colors : colorCodes)
+    {
+        cv::cvtColor(image.getImage(), transitions.getImage(), colors);
+        transitions.showImage();
+    }
 }
