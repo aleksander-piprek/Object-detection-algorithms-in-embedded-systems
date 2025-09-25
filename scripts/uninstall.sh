aae6b2acee1b5cef4f2e9e77b416934a2aa75143
Aleksander Piprek <kerpipkelo@gmail.com>
2025-09-25 14:07:49 +0200

    Updated uninstall script

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -6,38 +6,38 @@ set -e
 
 ### Uninstall CUDA 
 
-echo "Uninstalling CUDA..." | tee -a uninstall_log.txt
+echo "Uninstalling CUDA..." 
 sudo dpkg --remove --force-all cuda-repo-ubuntu2404-12-6-local
-sudo apt purge --autoremove cuda* nvidia* -y | tee -a uninstall_log.txt
-sudo apt autoremove -y | tee -a uninstall_log.txt
-sudo rm -rf /usr/local/cuda* | tee -a uninstall_log.txt
-sudo rm -rf /etc/apt/sources.list.d/cuda* | tee -a uninstall_log.txt
-sudo rm -rf /var/cuda-repo* | tee -a uninstall_log.txt
-sudo rm -f /etc/apt/preferences.d/cuda-repository-pin* | tee -a uninstall_log.txt
-sudo apt-get update | tee -a uninstall_log.txt
+sudo apt purge --autoremove cuda* nvidia* -y 
+sudo apt autoremove -y 
+sudo rm -rf /usr/local/cuda* 
+sudo rm -rf /etc/apt/sources.list.d/cuda* 
+sudo rm -rf /var/cuda-repo* 
+sudo rm -f /etc/apt/preferences.d/cuda-repository-pin* 
+sudo apt-get update 
 
 ### Uninstall cuDNN
 
-echo "Uninstalling cuDNN..." | tee -a uninstall_log.txt
-sudo apt purge --autoremove cudnn* libcudnn* -y | tee -a uninstall_log.txt
-sudo apt autoremove -y | tee -a uninstall_log.txt
-sudo rm -rf /usr/lib/x86_64-linux-gnu/libcudnn* | tee -a uninstall_log.txt
-sudo rm -rf /usr/include/x86_64-linux-gnu/cudnn* | tee -a uninstall_log.txt
-sudo rm -rf /var/cudnn-local-repo* | tee -a uninstall_log.txt
-sudo apt-get update | tee -a uninstall_log.txt
+echo "Uninstalling cuDNN..." 
+sudo apt purge --autoremove cudnn* libcudnn* -y 
+sudo apt autoremove -y 
+sudo rm -rf /usr/lib/x86_64-linux-gnu/libcudnn* 
+sudo rm -rf /usr/include/x86_64-linux-gnu/cudnn* 
+sudo rm -rf /var/cudnn-local-repo* 
+sudo apt-get update 
 
 ### Uninstall TensorRT
 
-echo "Uninstalling TensorRT..." | tee -a uninstall_log.txt
-sudo dpkg --remove --force-all nv-tensorrt-local-repo-ubuntu2204-10.3.0-cuda-12.5
-sudo apt purge --autoremove tensorrt libnvinfer* libnvparsers* libnvonnx* -y | tee -a uninstall_log.txt
-sudo apt autoremove -y | tee -a uninstall_log.txt
-sudo rm -rf /usr/src/tensorrt | tee -a uninstall_log.txt
-sudo rm -rf /usr/lib/x86_64-linux-gnu/libnvinfer* | tee -a uninstall_log.txt
-sudo rm -rf /usr/include/x86_64-linux-gnu/Nv* | tee -a uninstall_log.txt
-sudo rm -rf /var/nv-tensorrt-local-repo* | tee -a uninstall_log.txt
-sudo rm -f /etc/apt/preferences.d/tensorrt-pin | tee -a uninstall_log.txt
-sudo apt-get update | tee -a uninstall_log.txt
+echo "Uninstalling TensorRT..." 
+sudo dpkg --remove --force-all nv-tensorrt-local-repo-ubuntu2204-10.3.0-cuda-12.5_1.0-1
+sudo apt purge --autoremove tensorrt libnvinfer* libnvparsers* libnvonnx* -y 
+sudo apt autoremove -y 
+sudo rm -rf /usr/src/tensorrt 
+sudo rm -rf /usr/lib/x86_64-linux-gnu/libnvinfer* 
+sudo rm -rf /usr/include/x86_64-linux-gnu/Nv* 
+sudo rm -rf /var/nv-tensorrt-local-repo* 
+sudo rm -f /etc/apt/preferences.d/tensorrt-pin 
+sudo apt-get update 
 
 sed -i '/cuda/d' ~/.bashrc
 sed -i '/tensorrt/d' ~/.bashrc
